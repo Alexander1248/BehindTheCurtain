7150dd38ca914ba3f5bba222287d7e0f0e4accf7
Alexander <sasha.izmyloff@mail.ru>
2024-03-22 02:33:56 +0300

    Character Control

---
+++ Assets/Scripts/Spell/Spell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Spell")]
+public class Spell : ScriptableObject
+{
+    public GameObject prefab;
+    public int manaCost;
+    
+    
+
+}
---
+++ Assets/Scripts/SpellCaster.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class SpellCaster : MonoBehaviour
+{
+    public Spell spell;
+    
+}
