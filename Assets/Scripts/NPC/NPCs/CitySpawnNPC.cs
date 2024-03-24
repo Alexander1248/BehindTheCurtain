977961269f1d51c23029db0c9652e456e2e5d544
Alexander <sasha.izmyloff@mail.ru>
2024-03-25 00:34:39 +0300

    Fix

--- Assets/Scripts/NPC/NPCs/CitySpawnNPC.cs
+++ Assets/Scripts/NPC/NPCs/CitySpawnNPC.cs
@@ -1,29 +1,26 @@
 ﻿using UnityEngine;
 
-namespace NPCs
+public class CitySpawnNPC : SlowmoNPC
 {
-    public class CitySpawnNPC : SlowmoNPC
+    public GameObject house;
+    public GameObject monsters;
+    public GameObject fire;
+    public AudioSource scream;
+
+    protected override void OnTextLine(int line)
     {
-        public GameObject house;
-        public GameObject monsters;
-        public GameObject fire;
-        public AudioSource scream;
-        
-        protected override void OnTextLine(int line)
+        base.OnTextLine(line);
+        switch (line)
         {
-            base.OnTextLine(line);
-            switch (line)
-            {
-                case 2:
-                    house.SetActive(true);
-                    fire.SetActive(true);
-                    scream.Play();
-                    break;
-                case 3:
-                    monsters.SetActive(true);
-                    break;
-            }
-            
+            case 2:
+                house.SetActive(true);
+                fire.SetActive(true);
+                scream.Play();
+                break;
+            case 3:
+                monsters.SetActive(true);
+                break;
         }
+
     }
 }
