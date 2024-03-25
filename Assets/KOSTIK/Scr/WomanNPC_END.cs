295ea6c636e1e5b4a95821b08006628db2771fb0
KGoroshnikov <goroshnikovk1@gmail.com>
2024-03-25 05:21:34 +0300

    ABOBA

--- Assets/KOSTIK/Scr/WomanNPC_END.cs
+++ Assets/KOSTIK/Scr/WomanNPC_END.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WomanNPC_END : NPC
 {
@@ -13,6 +14,11 @@ public class WomanNPC_END : NPC
 
     void ENDGAME(){
         animatorFade.Play("FadeIn", 0, 0);
+        Invoke("changeScene", 3);
+    }
+
+    void changeScene(){
+        SceneManager.LoadScene(4);
     }
 
     protected override void OnTextLine(int line)
